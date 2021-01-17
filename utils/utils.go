57bf57264bf5940cd52cb77d71c48228daf36a5a
Yuto Mizutani <mizutani@fringe81.com>
2021-01-17 17:20:32 +0900

    add test

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,26 @@
 package utils
 
-import "errors"
+import (
+	"regexp"
+)
 
-func RemoveHeadCommentSymbol(s string)(string, error){
-	return ("tmp", errors.New("tmp"))
+func FormatComment(s string) string {
+	str := removeNewLineCharacter(s)
+	str = removeHeadSymbol(str)
+	return str
+}
+
+func removeHeadSymbol(s string) string {
+	// Remove comment symbols.
+	rep := regexp.MustCompile(`^//( |)`)
+	str := rep.ReplaceAllString(s, "")
+
+	return str
+}
+
+func removeNewLineCharacter(s string) string {
+	// Remove `\n//`.
+	rep := regexp.MustCompile(`\n//( |)`)
+	str := rep.ReplaceAllString(s, " ")
+	return str
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -5,10 +5,32 @@ import (
 	"testing"
 )
 
-func TestRemoveHeadCommentSymbol(t *testing.T){
-	result, _:= RemoveHeadCommentSymbol("//This is test.")
+func TestRemoveHeadCommentSymbol(t *testing.T) {
+	result := removeHeadSymbol("// This is test.")
+	if result != "This is test." {
+		log.Fatal("error")
+	}
+
+	result = removeHeadSymbol("//This is test.")
 	if result != "This is test." {
 		log.Fatal("error")
 	}
 }
 
+func TestRemoveNewLineCharacter(t *testing.T) {
+	s := `This is first line and
+// this is second line.`
+	result := removeNewLineCharacter(s)
+	if result != "This is first line and this is second line." {
+		log.Fatal(result)
+	}
+}
+
+func TestFormatComment(t *testing.T) {
+	s := `// This is first line and
+// this is second line.`
+	result := FormatComment(s)
+	if result != "This is first line and this is second line." {
+		log.Fatal(result)
+	}
+}
